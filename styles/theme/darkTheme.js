fca2f799b3f46193d6d8126e47b6495d899be5fd
Mehul Tanwar <mehultw.cs@gmail.com>
2022-07-31 18:44:32 -0300

    Init with TWcss MUI
    Inital

---
+++ styles/theme/darkTheme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+        main: '#fff',
+    },
+    mode: "dark",
+    type: "dark",
+  },
+});
+
+export default darkTheme;
